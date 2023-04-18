c9fc62cd77c45c9355d8c41497324284f074b3c7
Simon Chervenak <sarras305@gmail.com>
2023-04-18 15:46:48 +0200

    make deck

--- learning-scala/src/main/scala/OhHell.scala
+++ learning-scala/src/main/scala/OhHell.scala
@@ -3,18 +3,39 @@ import scala.util.Random
 val suits: List[String] = List("♠", "♥", "♦", "♣")
 val values: Range = Range.inclusive(2, 14)
 
-class Card(val suit: String, val value: Int) {
+class Card(var suit: String, var value: Int) {
   override def toString = value + " of " + suit
 }
 
-var deck: List[Card] = List()
+class Player(var name: String, var hand: List[Card]) {
+  override def toString = name + " has " + hand
+}
+
+class Deck(var cards: List[Card] = List()) {
+  def makeDeck(): Unit = {
+    cards = List()
+    for (s <- suits) {
+      for (v <- values) {
+        val card = Card(s, v)
+        cards = card :: cards
+      }
+    }
+    scala.util.Random.shuffle(cards)  
+  }
+
+  def deal(players: List[Player], nCards: Int): List[Player] = {
+    var newPlayers: List[Player] = List()
+    for (p <- players) {
+      val (newHand, newDeck) = cards.splitAt(nCards)
+      newPlayers = Player(p.name, newHand) :: newPlayers
+    }
+    newPlayers
+  }
+
+  override def toString = cards.toString
+}
+
 
 @main def main = {
-  for (s <- suits) {
-    for (v <- values) {
-      val card = Card(s, v)
-      deck = card :: deck
-    }
-  }
-  scala.util.Random.shuffle(deck)
+  val deck = Deck()
 }
